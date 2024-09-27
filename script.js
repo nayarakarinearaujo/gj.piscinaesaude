b539af2d5a0501d3129a29a27a5d45594478316a
Nayara Karine <149000384+nayarakarinearaujo@users.noreply.github.com>
2024-09-27 11:55:06 -0300

    Ajustando a mensagem de envio do formulário

--- script.js
+++ script.js
@@ -1,75 +1,72 @@
 // Classe responsável por gerenciar o carrossel de imagens
 class Carousel {
     constructor() {
-        this.slides = document.querySelectorAll('.carousel-slide'); // Seleciona todos os slides
-        this.currentSlide = 0; // Inicializa o índice do slide atual
-        this.intervalId = null; // Inicializa o ID do intervalo para controle de animação
+        this.slides = document.querySelectorAll('.carousel-slide'); 
+        this.currentSlide = 0; 
+        this.intervalId = null; 
     }
 
     // Método para mostrar o próximo slide
     nextSlide() {
-        this.slides[this.currentSlide].classList.remove('show'); // Remove a classe 'show' do slide atual
-        this.currentSlide = (this.currentSlide + 1) % this.slides.length; // Atualiza o índice do próximo slide
-        this.slides[this.currentSlide].classList.add('show'); // Adiciona a classe 'show' ao próximo slide
+        this.slides[this.currentSlide].classList.remove('show'); 
+        this.currentSlide = (this.currentSlide + 1) % this.slides.length; 
+        this.slides[this.currentSlide].classList.add('show'); 
     }
 
     // Método para iniciar a animação do carrossel
     startCarousel() {
-        this.intervalId = setInterval(() => this.nextSlide(), 2000); // Chama nextSlide a cada 2 segundos
+        this.intervalId = setInterval(() => this.nextSlide(), 2000); 
     }
 
     // Método para parar a animação do carrossel
     stopCarousel() {
-        clearInterval(this.intervalId); // Limpa o intervalo para interromper a animação
+        clearInterval(this.intervalId); 
     }
 }
 
-// Classe responsável por lidar com o formulário de contato
-class FormHandler {
-    constructor() {
-        this.phoneNumber = "5519999606402"; // Número de telefone para o qual enviar a mensagem
-        this.form = document.getElementById("contatoForm"); // Obtém o formulário de contato
-        this.form.addEventListener("submit", this.handleFormSubmit.bind(this)); // Adiciona um ouvinte de evento para o envio do formulário
-    }
 
-    // Método para enviar a mensagem para o WhatsApp
-    sendToWhatsApp(message) {
-        const whatsappMessage = encodeURIComponent(message); // Codifica a mensagem para o formato de URL
-        window.open("https://api.whatsapp.com/send?phone=" + this.phoneNumber + "&text=" + whatsappMessage); // Abre a URL no WhatsApp Web
-    }
-
-    // Método para lidar com o envio do formulário
-    handleFormSubmit(event) {
-        event.preventDefault(); // Impede o comportamento padrão de envio do formulário
-
-        // Obtém os valores dos campos do formulário
-        const name = document.getElementById("name").value;
-        const email = document.getElementById("email").value;
-        const phone = document.getElementById("phone").value;
-        const message = document.getElementById("message").value;
-
-        // Constrói a mensagem a ser enviada
-        const whatsappMessage = `Olá! Meu nome é ${name}. \n` +
-        `Gostaria de saber mais sobre os serviços. \n` +
-        `Aqui estão meus detalhes: \n` +
-        `E-mail: ${email} \n` +
-        `Telefone: ${phone} \n` +
-        `Mensagem: ${message} \n\n` +
-        `Aguardo seu retorno! Obrigado!`;
-        // Envia a mensagem para o WhatsApp
-        this.sendToWhatsApp(whatsappMessage);
-    }
+// Função para enviar a mensagem para o WhatsApp
+function sendToWhatsApp(message) {
+    const phoneNumber = "5519999606402"; 
+    const whatsappMessage = encodeURIComponent(message); 
+    window.open(`https://api.whatsapp.com/send?phone=${phoneNumber}&text=${whatsappMessage}`); 
 }
 
-// Evento que ocorre quando o DOM está completamente carregado
-document.addEventListener('DOMContentLoaded', () => {
-    // Cria uma instância da classe Carousel e inicia a animação
-    const carousel = new Carousel();
-    carousel.startCarousel();
+// Função para lidar com o envio do formulário
+function handleFormSubmit(event) {
+    event.preventDefault(); 
 
-    // Cria uma instância da classe FormHandler para lidar com o formulário de contato
-    const formHandler = new FormHandler();
+    // Obtém os valores dos campos do formulário
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const phone = document.getElementById("phone").value;
+    const message = document.getElementById("message").value;
+
+    // Verifica se os campos foram preenchidos
+    if (!name || !email || !phone || !message) {
+        alert("Por favor, preencha todos os campos.");
+        return;
+    }
+
+    // Constrói a mensagem a ser enviada
+    const whatsappMessage = `Olá! Meu nome é ${name}. \n` +
+    `Gostaria de saber mais sobre os serviços. \n` +
+    `Aqui estão meus detalhes: \n` +
+    `E-mail: ${email} \n` +
+    `Telefone: ${phone} \n` +
+    `Mensagem: ${message} \n\n` +
+    `Aguardo seu retorno! Obrigado!`;
+    
+    sendToWhatsApp(whatsappMessage);
+}
+
+// Adiciona o ouvinte de evento ao formulário quando a página é carregada
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById("contatoForm"); 
+    form.addEventListener("submit", handleFormSubmit); 
 });
+
+
 
 //Scroll suave do navbar para section
 
